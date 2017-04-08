8dd0ed889284fe39543b738cf7a98ed2aa7cb1fa
Vlad Pnevsk <vlad.007.p@gmail.com>
2017-04-08 21:54:37 +0300

    first commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+    public float Speed;
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0f);
+        _rb.AddForce(direction * Speed);
+    }
+}
